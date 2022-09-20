8a8c5d901401cb69b4e22b8a0f8bc32f9c8e95aa
seth austin <gingivere15@gmail.com>
2022-09-20 02:26:12 -0400

    added seed definition and some cleanup

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ url = "http://127.0.0.1:7860/api/predict"
 
 helpstring = "Hi! For a simple request, you can type something like \"!dale firetruck\"\n" \
              "More complicated requests have the following options:\n\n" \
-             "conform=1-30, describes how much the AI should conform to the prompt. Defaults to 10\n" \
+             "conform=1-30, describes how much the AI should conform to the prompt. Defaults to 7\n" \
              "num=1-16, describes how many pictures to generate. Defaults to 1\n" \
              "samples=1-100, describes how many times the ai should run over the picture. Defaults to 20\n" \
              "res=1-1600x1-1600, describes the resolution of the image. Defaults to 512x512\n" \
@@ -54,8 +54,37 @@ async def on_message(message):
         conformind=10
         resxind=16
         resyind=17
+        seedind=11
         denoiseBool=False
         if len(message.attachments)>0:
+            if len(words)>1 and words[0] == "upscale":
+                f=open('updata.json')
+                postObj = json.load(f)
+                f.close()
+                with open("output.png", "wb") as imgfile:
+                    imgfile.write(requests.get(message.attachments[0].url).content)
+                encodedattachment = base64.b64encode(open("output.png", "rb").read())
+                if os.path.exists("output.txt"):
+                    os.remove("output.txt")
+                with open("output.txt", "wb") as textfile:
+                    textfile.write(encodedattachment)
+                with open("output.txt", "r") as textfile:
+                    postObj['data'][0] = "data:image/png;base64," + textfile.read()
+                if words[1].isnumeric() and float(words[1]) <= 10:
+                    postObj['data'][5] = float(words[1])
+                response = requests.post(url, json=postObj)
+                # with open("outie.json","wb") as f:
+                #     f.write(response.json())
+                imgdata = base64.b64decode(response.json()['data'][0][0][22:])
+                filename = "testimg.png"
+                with open(filename, "wb") as f:
+                    f.write(imgdata)
+                with open(filename, 'rb') as f:
+                    picture = discord.File(f)
+                    await message.reply(file=picture)
+                await message.add_reaction("🔄")
+                await message.add_reaction("✅")
+                return
             f = open('imgdata.json')
             postObj = json.load(f)
             f.close()
@@ -85,16 +114,17 @@ async def on_message(message):
                 samples = word.split("=")[1]
                 if samples.isnumeric() and int(samples) <= 100:
                     postObj['data'][sampleind] = int(samples)
+                prompt = prompt.replace(word, "")
             if 'num=' in word:
                 numpics = word.split("=")[1]
                 if numpics.isnumeric() and int(numpics)<17:
                     postObj['data'][numind] = int(numpics)
-                prompt.replace(word,"")
+                prompt = prompt.replace(word,"")
             if 'conform=' in word:
                 conform = word.split("=")[1]
                 if conform.isnumeric() and int(conform) <= 100:
                     postObj['data'][conformind] = int(conform)
-                prompt.replace(word, "")
+                prompt = prompt.replace(word, "")
             if 'res=' in word:
                 resolution = word.split("=")[1]
                 resx = resolution.split("x")[1]
@@ -104,22 +134,28 @@ async def on_message(message):
                     postObj['data'][resyind] = int(resy)
                 else:
                     await message.channel.send("I am from the south and I'm really fucking stupid so I can't render images bigger than 1600x1600. Try using the upscaler (which doesn't exist yet)")
-                prompt.replace(word, "")
+                prompt = prompt.replace(word, "")
             if 'dn=' in word:
                 dn = word.split("=")[1]
                 if float(dn) <= 1 and denoiseBool:
                     postObj['data'][18]=float(dn)
                 prompt.replace(word,"")
+            if 'seed=' in word:
+                seed = word.split("=")[1]
+                postObj['data'][seedind] = int(seed)
+                prompt = prompt.replace(word, "")
 
+        print("prompt: "+prompt)
         postObj['data'][0] = prompt
         response = requests.post(url, json=postObj)
+        print("seed:" +json.dumps(response.json()).split("Seed:",1)[-1].split()[0][:-1])
         imgdata = base64.b64decode(response.json()['data'][0][0][22:])
         filename = "testimg.png"
         with open(filename, "wb") as f:
             f.write(imgdata)
         with open(filename, 'rb') as f:
             picture = discord.File(f)
-            await message.channel.send(file=picture)
+            await message.reply("Seed: "+json.dumps(response.json()).split("Seed:",1)[-1].split()[0][:-1], file=picture)
         await message.add_reaction("🔄")
         await message.add_reaction("✅")
         if "help" in message.content[6:].split()[0]:
